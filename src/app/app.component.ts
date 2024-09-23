03d3be1e46969bfb829bd28d13fe3b4b6f922e7e
Cedrico <cedrico.knoesel@t-online.de>
2024-09-23 11:57:55 +0200

    add first exercise with simple email form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {EmailReaderBasicComponent} from "./email-reader-basic/email-reader-basic.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EmailReaderBasicComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/email-reader-basic/email-reader-basic.component.ts
@@ -0,0 +1,45 @@
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {Email} from "../interfaces/email";
+import {FormsModule} from "@angular/forms";
+
+const EMPTY_EMAIL: Email = {
+  body: "", from: "", subject: "", to: ""
+}
+
+@Component({
+  selector: 'app-email-reader-basic',
+  standalone: true,
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './email-reader-basic.component.html',
+  styleUrl: './email-reader-basic.component.css'
+})
+export class EmailReaderBasicComponent implements OnInit {
+  email: Email;
+  @ViewChild('emailForm') emailForm: any;
+
+  constructor() {
+    this.email = {
+      body: "", from: "", subject: "", to: ""
+    }
+  }
+
+  ngOnInit(): void {
+    this.email = {
+      body: "", from: "", subject: "", to: ""
+    }
+  }
+
+  send(): void {
+    alert(`Sending the following message:
+From: ${this.email.from}
+To: ${this.email.to}
+Subject: ${this.email.subject}
+Body: ${this.email.body}`)
+  }
+
+  reset(): void {
+    this.emailForm.reset()
+  }
+}
